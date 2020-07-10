f2c33bc1fc000203e3b2279c06d1552f0510ddf4
kamila.gomes <kamila.br.gomes@hotmail.com>
2020-07-09 22:08:12 -0300

    ajustando url da base

--- src/http-common.js
+++ src/http-common.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 //Define a URL base da origem para consumo do servico
 export default axios.create({
-  baseURL: 'https://backend-grades-8081.herokuapp.com/',
+  baseURL: 'https://backend-grades-8081.herokuapp.com',
   headers: {
     'Content-type': 'application/json',
   },

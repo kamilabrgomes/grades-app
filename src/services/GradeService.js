d32fe84d35069082150577ccff70761f2bcd26be
kamila.gomes <kamila.br.gomes@hotmail.com>
2020-07-09 22:57:47 -0300

    ajustando url da base

--- src/services/GradeService.js
+++ src/services/GradeService.js
@@ -12,12 +12,12 @@ const create = (data) => {
   return http.post('/grade', data);
 };
 
-const update = (id, data) => {
-  return http.put(`/grade/${id}`, data);
+const update = (_id, data) => {
+  return http.put(`/grade/${_id}`, data);
 };
 
-const remove = (id) => {
-  return http.delete(`/grade/${id}`);
+const remove = (_id) => {
+  return http.delete(`/grade/${_id}`);
 };
 
 const removeAll = () => {
